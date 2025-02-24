640bb366903fa4c2bd5afadcfbe8389a4c9513ec
SoukainaLAKBICHI <soukaina.lakbichi-etu@etu.univh2c.ma>
2025-02-24 09:54:09 +0000

    mplémentation de interface IMetier

---
+++ src/Metier/MetierImpl.java
@@ -0,0 +1,18 @@
+package Metier;
+
+import Dao.IDao;
+
+public class MetierImpl implements IMetier {
+    private IDao dao; // le couplage faible
+
+
+    public MetierImpl(IDao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public double calcul() {
+        return dao.getData() * 2;
+    }
+}
+
